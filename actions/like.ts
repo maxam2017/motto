9d97c9d5a00c277c163f9ae78006bf70ae352998
maxam2017 <whk0313@gmail.com>
2024-05-13 18:59:13 +0800

    feat: integrate with upstash redis

--- actions/like.ts
+++ actions/like.ts
@@ -3,7 +3,11 @@
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
+import { createRedisClient } from "@/utils/redis";
+
 const ONE_YEAR = 60 * 60 * 24 * 365;
+
+const redisClient = createRedisClient();
 
 export async function toggle() {
   "use server";
@@ -11,8 +15,10 @@ export async function toggle() {
   const liked = cookies().get("liked")?.value === "true";
 
   if (liked) {
+    redisClient.incrby("likes", -1);
     cookies().set("liked", "false", { maxAge: ONE_YEAR });
   } else {
+    redisClient.incrby("likes", 1);
     cookies().set("liked", "true", { maxAge: ONE_YEAR });
   }
 
@@ -22,7 +28,12 @@ export async function toggle() {
 export async function countLikes() {
   "use server";
 
-  const liked = cookies().get("liked")?.value === "true";
+  const likes: string | null = await redisClient.get("likes");
 
-  return liked ? 2 : 1;
+  if (likes === null) {
+    await redisClient.set("likes", 0);
+    return 0;
+  }
+
+  return parseInt(likes);
 }
---
+++ utils/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from "@upstash/redis";
+
+export function createRedisClient() {
+  return new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  });
+}
