5a417be60f653e276938aa8af4eedc66d70f0bb5
maxam2017 <whk0313@gmail.com>
2024-05-13 19:20:20 +0800

    feat: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { ScrollText } from "lucide-react";
+
+import { cn } from "@/utils/cn";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,8 +21,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={cn("h-full flex flex-col", inter.className)}>
+        <header className="px-6 py-4 text-black">
+          <h1 className="flex space-x-2 items-center font-bold text-lg">
+            <ScrollText className="w-8 h-8" /> <span>Motto</span>
+          </h1>
+        </header>
+        {children}
+        <footer>
+          <div className="flex justify-center py-4 text-gray-500 text-xs">
+            <span>
+              &copy; {new Date().getFullYear()}{" "}
+              <a href="https://maxam.dev" className="text-blue-500">
+                maxam.dev
+              </a>
+            </span>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { cookies } from "next/headers";
 
-import { ScrollText } from "lucide-react";
-
 import { countLikes, toggle } from "@/actions/like";
 import { formatNumber } from "@/utils/formatNumber";
 
@@ -14,28 +12,21 @@ export default async function Page() {
   const likeCount = await countLikes();
 
   return (
-    <>
-      <header className="px-6 py-4 text-black">
-        <h1 className="flex space-x-2 items-center font-bold text-lg">
-          <ScrollText className="w-8 h-8" /> <span>Motto</span>
-        </h1>
-      </header>
-      <div className="flex flex-col items-center justify-center pt-60 font-mono">
-        <div className="text-2xl md:text-4xl !leading-snug tracking-tighter md:tracking-normal font-medium text-gray-800 pt-4 pl-8 pb-12 text-balance">
-          &quot;Be kind and patient. <br />
-          Try to understand those you don&apos;t understand.&quot;
-        </div>
-        <form className="flex flex-col items-center" action={toggle}>
-          <LikeButton className="flex items-center gap-2 group" liked={liked} />
-        </form>
-        <span className="text-xs mt-3 text-gray-500">
-          {liked
-            ? `You and ${formatNumber(likeCount - 1)} others liked this`
-            : `
+    <main className="flex flex-col items-center justify-center flex-1 font-mono">
+      <div className="text-2xl md:text-4xl !leading-snug tracking-tighter md:tracking-normal font-medium text-gray-800 pt-4 pl-8 pb-12 text-balance">
+        &quot;Be kind and patient. <br />
+        Try to understand those you don&apos;t understand.&quot;
+      </div>
+      <form className="flex flex-col items-center" action={toggle}>
+        <LikeButton className="flex items-center gap-2 group" liked={liked} />
+      </form>
+      <span className="text-xs mt-3 text-gray-500">
+        {liked
+          ? `You and ${formatNumber(likeCount - 1)} others liked this`
+          : `
           ${formatNumber(likeCount)} ${likeCount === 1 ? "person" : "people"} liked this
         `}
-        </span>
-      </div>
-    </>
+      </span>
+    </main>
   );
 }
